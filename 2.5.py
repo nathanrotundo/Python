afb0a9b06af0c17dcedeb5764f9b2b4b4ba22ffb
nathanrotundo <156851657+nathanrotundo@users.noreply.github.com>
2024-01-19 12:53:36 -0600

    Add files via upload

---
+++ 2.5.py
@@ -0,0 +1,11 @@
+# Define the size of the fuel tank in gallons
+tank_size = 15
+
+# Define the distance traveled in miles
+distance = 600
+
+# Calculate Miles Per Gallon (MPG) using the formula: MPG = distance / tank_size
+mpg = distance / tank_size
+
+# Display the calculated Miles Per Gallon
+print("Miles per gallon:", mpg)
