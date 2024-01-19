24095164056db0d1ded3e584595699aee3541dc6
nathanrotundo <156851657+nathanrotundo@users.noreply.github.com>
2024-01-19 11:40:04 -0600

    Add files via upload

---
+++ 2.2.py
@@ -0,0 +1,21 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+x = 50
+print("Literal:", x)
+
+# Integer
+y = -100
+print("Integer:", y)
+
+# Float
+z = 100
+print("Float:", z)
+
+# String
+name = "Nate"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
