6a73f0c49e75760826c545d82f0b613d3e4744a2
nathanrotundo <156851657+nathanrotundo@users.noreply.github.com>
2024-01-19 11:42:14 -0600

    Add files via upload

---
+++ 2.3.py
@@ -0,0 +1,5 @@
+# The Purpose of the program is to calculate and display the area of a retangle
+length = 3
+width = 4
+area = length * width 
+print("the area of the rectangle with a length of 3 and a width of 4 is 12")
