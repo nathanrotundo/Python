f27b296f009e63083a3541629a94a2615046db5a
nathanrotundo <156851657+nathanrotundo@users.noreply.github.com>
2024-02-29 18:26:09 -0600

    Add files via upload

---
+++ 4.6.a.2.py
@@ -0,0 +1,8 @@
+# tuple of programming classes    
+programming_classes = ('Intro to Python', 'Advanced Python' ,'Database Essentials' ,'Web Development Basics', 
+                           'Data Structures in Python', 'Web Design Fundamentals')
+for item in programming_classes:
+    #print the classes   
+    print(item, end= " ,")
+#print the amount of classes
+print("\nthere are", len(programming_classes),"programming classes")
