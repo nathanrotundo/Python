15e96e5ed0da9252a61037221046c0909d6a1b05
nathanrotundo <156851657+nathanrotundo@users.noreply.github.com>
2024-03-03 09:19:55 -0600

    Add files via upload

---
+++ 4.6.b.py
@@ -0,0 +1,25 @@
+def main():
+    # Nato Phonetic Alphabet 
+    nato_alphabet = {"A": "Alpha", "B": "Bravo","c": "Charlie", "D": "Delta", "E": "Echo"
+                 , "f": "Foxtrot", "G": "Golf", "H": "Hotel", "I": "India", "J": "Juliett"
+                 , "K": "Kilo", "L": "Lima", "M": "Mike", "N": "November", "O": "Oscar"
+                 , "P": "Papa", "Q": "Quebec", "R": "Romeo", "S": "Sierra", "T": "Tango"
+                 , "U": "Uniform", "V": "Victor", "W": "Whiskey", "X": "X-ray", "Y": "Yankee", "Z": "Zulu"}
+
+
+# spelling program
+    def spell_word():
+        # asks user to input a word
+        user_word = input("Enter a word:").upper() 
+        
+        for key in user_word:
+            # match each letter to its phonetic counterpart 
+            if key in nato_alphabet:
+                print(nato_alphabet[key])
+            # prompt if user inouts something other than a letter
+            if key not in nato_alphabet:
+                print("only enter letters A-Z")
+
+    spell_word()
+
+main()
