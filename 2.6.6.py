7bf665887d8789b5c83acd1799fe9b10c88ba254
nathanrotundo <156851657+nathanrotundo@users.noreply.github.com>
2024-04-01 10:25:33 -0500

    Add files via upload

---
+++ 2.6.6.py
@@ -0,0 +1,35 @@
+def main():
+    try:
+        # Create a list to store flowers in
+        flowers = []
+        print("Enter flower names. When you are finished, type 'done'.")
+        
+        while True:
+            flower = input("Enter a flower name: ")
+            if flower == "done":
+                break  # Breaks the loop once user inputs 'done'
+            flowers.append(flower)  # Adds flowers to the empty list
+        
+        flowers.sort()  # Sorts the list of flowers alphabetically
+        print("List of flowers:")
+        for index, flower in enumerate(flowers, start=1):  # Using enumerate to get both index and flower
+            print(f"{index}. {flower}")
+        
+        search = input("Type a flower to search for: ")
+        if search in flowers:
+            print(f"{search} found in the list.")
+        else:
+            print(f"{search} not found in the list.")
+        
+        try:
+            number = int(input("Enter number to access the corresponding flower: "))
+            if number in range(1, len(flowers)+1):  # Check if number is within valid range
+                print(f"Flower number {number}: {flowers[number - 1]}")
+            else:
+                print("You must input a valid number")
+        except ValueError:
+            print("Oops! Please enter a number.")
+    except Exception:
+        print("Oops! Looks like an error occurred.")
+    
+main()
