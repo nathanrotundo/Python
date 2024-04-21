bb87e7d88aa5064f9ae2d2939f189f2100df6a2a
nathanrotundo <156851657+nathanrotundo@users.noreply.github.com>
2024-04-21 09:35:28 -0500

    Add files via upload

---
+++ personal_diary.py
@@ -0,0 +1,16 @@
+def main():
+    # Prompt the user to enter the current date and time and a journal entry
+    date_time = input("Enter the current date and time example: ")
+    diary_entry = input("Diary entry: ")
+
+    # Open or create a file named diary.txt
+    with open("diary.txt", "a") as file:
+        # Write the provided date and time into the file
+        file.write(date_time)
+        file.write("\n")  # Write a blank line after date and time
+        # Write the diary entry into the file
+        file.write(diary_entry)
+        file.write("\n")  # Write a blank line after entry
+
+
+main()
